ea2f5dfc50c776f19b2c3fb347e0a4d7462f0290
ahmetkecici 99 <ahmetkecici99@gmail.com>
2023-08-09 10:57:40 +0300

    Add project files.

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatingAppContext _context;
+        public UsersController(DatingAppContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetAll()
+        {
+            var data= await _context.AppUsers.ToListAsync( );
+            return data;
+        }
+
+    }
+}
---
+++ Api/Data/DatingAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DatingAppContext:DbContext
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public DatingAppContext(DbContextOptions options):base(options)
+        {
+            
+        }
+    }
+}
